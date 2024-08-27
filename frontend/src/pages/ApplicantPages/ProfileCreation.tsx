c56f5096c69b5461b789c965c96e3c707d568bbb
Hasti Gabani <hastigabani1109@gmail.com>
2024-08-27 22:52:50 +0530

    added create profile api to ui

--- frontend/src/pages/ApplicantPages/ProfileCreation.tsx
+++ frontend/src/pages/ApplicantPages/ProfileCreation.tsx
@@ -3,13 +3,17 @@ import { IoCloudUploadOutline, IoArrowForward } from "react-icons/io5";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import Logo from "../../components/Logo";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 type FormFields = {
+  resume: string;
   name: string;
-  phone: string;
+  //email: string;
+  phoneNumber: string;
   city: string;
-  years: number;
-  months: number;
+  header: string;
+  description: string;
   job_title_1: string;
   job_title_2: string;
   job_title_3: string;
@@ -17,9 +21,15 @@ type FormFields = {
 
 const CandidateProfile = () =>{
 
+  const location = useLocation();
+
+  // Extract query parameters from the URL
+  const queryParams = new URLSearchParams(location.search);
+  const email = queryParams.get('email') || '';
+
   const inputClassnames = "py-2 border-[1px] border-gray-300 rounded-md placeholder:text-sm text-sm px-2 focus:border-black focus:outline-none mt-1 w-full mb-1";
 
-  const buttonClassnames = "cursor-pointer inline-flex items-center justify-center text-white bg-primary hover:bg-primary-variant"
+  const buttonClassnames = "cursor-pointer inline-flex items-center justify-center text-white bg-primary hover:bg-primary-variant";
 
   const [fileName, setFileName] = useState<string>('Choose File');
 
@@ -29,30 +39,53 @@ const CandidateProfile = () =>{
       ? fileInput.files[0].name
       : 'Choose File';
     setFileName(selectedFileName);
+    setValue('resume', selectedFileName);
   };
 
   const { 
     register, 
     handleSubmit, 
     formState:{errors},
-    setError
+    setValue,
+    getValues
   } = useForm<FormFields>();
 
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
     try {
-      //simulation for checking db data
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      console.log(data);
-      //simutlation for getting an error from backend
-      throw new Error("Error updating profile. Try again later.");
+      // Combine job titles into an array
+      const interestedRoles = [
+        data.job_title_1,
+        data.job_title_2,
+        data.job_title_3,
+      ].filter(Boolean); // Remove any empty or undefined values
+
+      // Update the data object to include the interestedRoles array
+      const updatedData = {
+        name: getValues('name'),
+        phoneNumber: getValues('phoneNumber'),
+        city: getValues('city'),
+        header: getValues('header'),
+        description: getValues('description'),
+        interestedRoles,
+        email,
+        resume: fileName
+      };
+      console.log(updatedData);
+      //setValue('email', email);
+      const response = await axios.post(
+        `http://localhost:5000/api/applicants`,
+        updatedData, 
+        { headers: {
+          'Content-Type': 'application/json',
+        }})
+      //await new Promise((resolve) => setTimeout(resolve, 1000));
+      //console.log(data);
+      if(!(response.status == 201))
+        throw new Error(response.data.message);
+      else 
+        toast.success("Profile created successfully.")
     } catch (e) {
-      toast.error("Error signing in. Try again later.");
-      if (e instanceof Error) {
-        setError("root", {
-          type: "manual",
-          message: e.message,
-        });
-      }
+      toast.error(e + ". Try again later.");
     }
   }
 
@@ -63,8 +96,8 @@ const CandidateProfile = () =>{
           <Logo/>
           
           <div className="flex items-center md:mr-8">
-            <h3 className="text-[12px] md:text-sm mr-2">hastigabani1109@gmail.com</h3>
-            <div className="w-8 h-8 md:w-10 md:h-10 rounded-full border-2 border-white flex justify-center items-center bg-white text-primary font-semibold">H</div>
+            <h3 className="text-[12px] md:text-sm mr-2">{email}</h3>
+            <div className="w-8 h-8 md:w-10 md:h-10 rounded-full border-2 border-white flex justify-center items-center bg-white text-primary font-semibold">{email?.charAt(0)}</div>
           </div>
         </div>
       </header>
@@ -78,7 +111,19 @@ const CandidateProfile = () =>{
               <p className="text-base font-semibold">Upload your Resume/CV <span className="text-red-500">*</span></p>
               <p className="text-xs text-gray-400 mb-3">Only allowed .PDF, .DOC, .DOCX and max 4 MB file size</p>
               <div className="relative inline-block">
-                <input type="file" id="fileInput" className="hidden" onChange={handleFileChange}/>
+              <input
+                type="file"
+                id="fileInput"
+                className="hidden"
+                {...register('resume', {
+                  //required: 'Resume is required',
+                  validate: {
+                    // Add custom validations here if needed
+                  },
+                })}
+                onChange={handleFileChange}
+              />
+              {errors.resume && <p className="text-red-500">{errors.resume.message}</p>}
                 <label htmlFor="fileInput" className={`${buttonClassnames} rounded-md px-4 py-2 text-sm font-medium`}>
                   <IoCloudUploadOutline className="mr-2 text-lg"/>{fileName}
                 </label>
@@ -101,20 +146,20 @@ const CandidateProfile = () =>{
             />
             {errors.name && <div className="mt-1 text-error text-sm text-left">{errors.name.message}</div>}
 
-            {/* phone no */}
+            {/* phoneNumber no */}
             <p className='text-base mt-5 font-semibold'>Phone number <span className="text-red-500">*</span></p>
             <input
-            {...register("phone", {
+            {...register("phoneNumber", {
               required: "Phone number is required.",
               validate: (value) => {
-                return /^\d{10}$/.test(value) || "Please enter a valid phone number.";
+                return /^\d{10}$/.test(value) || "Please enter a valid phoneNumber number.";
               }
             })}
               type="text"
               placeholder="e.g. +91 99999 88888"
               className={inputClassnames}
             />
-            {errors.phone && <div className="mt-1 text-error text-sm text-left">{errors.phone.message}</div>}
+            {errors.phoneNumber && <div className="mt-1 text-error text-sm text-left">{errors.phoneNumber.message}</div>}
 
             {/* city */}
             <p className='text-base mt-5 font-semibold'>Current city <span className="text-red-500">*</span></p>
@@ -130,6 +175,29 @@ const CandidateProfile = () =>{
               className={inputClassnames}
             />
             {errors.city && <div className="mt-1 text-error text-sm text-left">{errors.city.message}</div>}
+
+            {/* header */}
+            <p className='text-base mt-5 font-semibold'>Header that describes you <span className="text-red-500">*</span></p>
+            <input
+            {...register("header",{
+              required: "Header is required.",
+            })}
+              type="text"
+              placeholder="e.g. Android Developer"
+              className={inputClassnames}
+            />
+            {errors.header && <div className="mt-1 text-error text-sm text-left">{errors.header.message}</div>}
+
+            {/* description */}
+            <p className='text-base mt-5 font-semibold'>A brief description about you<span className="text-red-500">*</span></p>
+            <textarea
+              {...register("description",{
+                required: "Description is required.",
+              })}
+                placeholder="e.g. Type something..."
+                className={inputClassnames}
+            />
+            {errors.description && <div className="mt-1 text-error text-sm text-left">{errors.description.message}</div>}
 
             {/* job titles */}
             <p className='text-base mt-5 font-semibold'>Tell us 3 positions you are interested in<span className="text-red-500">*</span></p>

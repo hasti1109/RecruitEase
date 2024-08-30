db3ccae4133f95eefb93f6b9c32538ab0f504742
Hasti Gabani <hastigabani1109@gmail.com>
2024-08-30 16:55:13 +0530

    added page

---
+++ frontend/src/pages/ApplicantPages/SavedJobsPage.tsx
@@ -0,0 +1,9 @@
+const SavedJobsPage = () => {
+  return (
+    <div className='mt-40 font-semibold text-2xl flex justify-center items-center'>
+      Saved jobs will appear here.
+    </div>
+  )
+}
+
+export default SavedJobsPage
